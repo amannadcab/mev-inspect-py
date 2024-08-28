1de06997e4d51846803a99bb872998b5c892d39d
amankumarp <aktech4u2@gmail.com>
2024-08-28 15:29:41 +0530

    fix server timeout issue

--- api/db.js
+++ api/db.js
@@ -6,7 +6,21 @@ const pool = new Pool({
   host: "localhost",
   database: "mev_inspect",
   password: "postgres",
+  max: 150,
+  keepAlive: true,
+  idleTimeoutMillis: 60000, 
+  connectionTimeoutMillis: 60000,
   port: 5432,
+});
+
+pool.on('error', (err) => {
+  ch.logger('Unexpected error on idle pool client', err);
+});
+
+pool.connect((err) => {
+  if (err) {
+    ch.logger('Error while logging into DataBase', err.stack);
+  }
 });
 
 module.exports = pool;
