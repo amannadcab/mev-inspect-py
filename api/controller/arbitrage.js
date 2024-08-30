efa067cb4cdd4b612c065282d6b9e84495314cff
amankumarp <aktech4u2@gmail.com>
2024-08-30 11:19:56 +0530

    update

--- api/controller/arbitrage.js
+++ api/controller/arbitrage.js
@@ -165,7 +165,12 @@ LIMIT 100;
       obj.contract_address = d.transaction_to_address;
       obj.transaction_hash = d.transaction_hash;
       obj.protocols = d.protocols;
-      obj.transfers = d.transfrs.map((d) => d.token_address);
+      obj.transfers = d.transfrs.map((d) => d.token_address).filter(
+        (value, index, self) =>
+          self.findIndex(
+            (obj) => JSON.stringify(obj) === JSON.stringify(value)
+          ) === index
+      );
       obj.block_number = d.block_number;
       obj.profit_token_address = d.profit_token_address;
       obj.gas_cost = ((d.gas_used * d.gas_price) / 1e18) * prices[wbnb];
@@ -481,142 +486,8 @@ async function loadPrice() {
   );
 }
 
-// async function getTopFiveRecord(req, res) {
-//   try {
-//     const query = `
-//       SELECT
-//     a.*,
-//     mev.gas_used,
-//     mev.gas_price,
-//     mev.coinbase_transfer,
-//     mev.miner_address,
-//     mev.base_fee_per_gas,
-//     JSON_AGG(
-//         JSON_BUILD_OBJECT(
-//             'from_address', t.from_address,
-//             'to_address', t.to_address,
-//             'token_address', t.token_address,
-//             'amount', t.amount
-//         )
-//     ) AS transfrs,
-//     mp.transaction_from_address,  -- Directly selecting transaction_from_address
-//     mp.transaction_to_address     -- Directly selecting transaction_to_address
-// FROM
-//     arbitrages a
-// LEFT JOIN
-//     transfers t
-//     ON a.transaction_hash = t.transaction_hash
-// JOIN
-//     mev_summary mev
-//     ON a.transaction_hash = mev.transaction_hash
-// LEFT JOIN
-//     miner_payments mp
-//     ON a.transaction_hash = mp.transaction_hash
-// WHERE
-//     a.transaction_hash NOT IN (
-//         SELECT
-//             transaction_hash
-//         FROM
-//             classified_traces
-//         WHERE
-//             error = 'Reverted'
-//     )
-// GROUP BY
-//     a.id,
-//     mev.gas_used,
-//     mev.gas_price,
-//     mev.coinbase_transfer,
-//     mev.miner_address,
-//     mev.base_fee_per_gas,
-//     mp.transaction_from_address,
-//     mp.transaction_to_address
-
-//     ORDER BY a.profit_amount DESC
-//       LIMIT 5
-//     `;
-//     const client = await pool.connect();
-//     const result = await client.query(query); // Execute the query
-//     const arbData = result.rows;
-//     ("SELECT * FROM  sandwiches ORDER BY profit_amount DESC LIMIT 5;");
-//     const querySand = `
-//       SELECT
-//      s.*,
-//      sd.profit_amount,
-//      sd.profit_token_address,
-//      JSON_BUILD_OBJECT(
-//          'frontrun_swap_transaction_hash', sd.frontrun_swap_transaction_hash,
-//          'backrun_swap_transaction_hash', sd.backrun_swap_transaction_hash,
-//          'frontrun_swap_details', sf.*,
-//          'backrun_swap_details', sb.*
-//      ) AS sandwiches_data,
-//      JSON_BUILD_OBJECT(
-//          'transfer_front_data', tf.*,
-//          'transfer_back_data', tb.*
-//      ) AS trnsfrr,
-//     JSON_BUILD_OBJECT(
-//          'miner_payments_front', mpf.*,
-//          'miner_payments_back', mpb.*
-//      ) AS minerpayments
-//   FROM
-//      swaps s
-//   LEFT JOIN
-//      sandwiches sd
-//      ON s.transaction_hash = sd.frontrun_swap_transaction_hash
-//      OR s.transaction_hash = sd.backrun_swap_transaction_hash
-//   LEFT JOIN
-//      transfers tf
-//      ON sd.frontrun_swap_transaction_hash = tf.transaction_hash
-//   LEFT JOIN
-//      transfers tb
-//      ON sd.backrun_swap_transaction_hash = tb.transaction_hash
-//   LEFT JOIN
-//      swaps sf
-//      ON sd.frontrun_swap_transaction_hash = sf.transaction_hash
-//   LEFT JOIN
-//      swaps sb
-//      ON sd.backrun_swap_transaction_hash = sb.transaction_hash
-//   LEFT JOIN
-//       miner_payments mpf
-//       ON sd.frontrun_swap_transaction_hash = mpf.transaction_hash
-//   LEFT JOIN
-//       miner_payments mpb
-//       ON sd.backrun_swap_transaction_hash = mpb.transaction_hash
-//   WHERE
-//      (sd.frontrun_swap_transaction_hash IS NOT NULL
-//      OR sd.backrun_swap_transaction_hash IS NOT NULL)
-//      AND sd.frontrun_swap_transaction_hash NOT IN (
-//           SELECT
-//               transaction_hash
-//           FROM
-//               classified_traces
-//           WHERE
-//               error = 'Reverted'
-//       )
-//      AND sd.backrun_swap_transaction_hash NOT IN (
-//           SELECT
-//               transaction_hash
-//           FROM
-//               classified_traces
-//           WHERE
-//               error = 'Reverted'
-//       )
-//       LIMIT 5;
-//     `;
-//     const resultSand = await client.query(querySand); // Execute the query
-//     const sandData = resultSand.rows;
-
-//     return res.json({
-//       arbitrage: { arbData, type: "arbitrage" },
-//       sandwich: { sandData, type: "sandwich" },
-//     });
-//   } catch (error) {
-//     console.error("Error fetching top 5 records:", error);
-//   }
-// }
-
 
 loadPrice();
-// setInterval(loadPrice,600000);
 
 module.exports = {
   getDailyTransaction,
